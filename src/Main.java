9833a92ed5fe9d234ec140fb58b9ffa57d510939
Aleksandr <hitrovaleks2014@gmail.com>
2023-03-09 18:28:22 +0300

    new commit+

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        float BMI = (float) service.calculate(82, 185);
+        float BMI = service.calculate(82, 185);
         System.out.println(BMI);
 
     }
